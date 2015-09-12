17b22908f5af4d065fa8851a563a6d26d459adad
Gianluca Formica <gianlucaformica@outlook.com>
2015-09-12 22:00:15 +0100

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -1,6 +1,8 @@
+## Code below requires that household_power_consumption.txt is in the working directory
 system.time ( hpc <- read.csv.sql(file = "household_power_consumption.txt", sql = "select * from file where Date in ('1/2/2007', '2/2/2007')", header = TRUE, sep = ";") )
 datetime<-as.POSIXct(paste(hpc$Date, hpc$Time), format="%d/%m/%Y %H:%M:%S")
 par(mfrow = c(1, 1))
+
 ##plot3
 plot(datetime,hpc$Sub_metering_1,type="n",ylab = "Energy sub metering", xlab = "")
 lines(datetime, hpc$Sub_metering_1,col="black")
