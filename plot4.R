4c55edb1c012d61835ecbb1838b0f2ffcb2e7b10
Gianluca Formica <gianlucaformica@outlook.com>
2015-09-12 22:01:01 +0100

    Update plot4.R

--- plot4.R
+++ plot4.R
@@ -1,5 +1,7 @@
+## Code below requires that household_power_consumption.txt is in the working directory
 system.time ( hpc <- read.csv.sql(file = "household_power_consumption.txt", sql = "select * from file where Date in ('1/2/2007', '2/2/2007')", header = TRUE, sep = ";") )
 datetime<-as.POSIXct(paste(hpc$Date, hpc$Time), format="%d/%m/%Y %H:%M:%S")
+
 ##plot4
 par(mfrow = c(2, 2))
 datetime<-as.POSIXct(paste(hpc$Date, hpc$Time), format="%d/%m/%Y %H:%M:%S")
