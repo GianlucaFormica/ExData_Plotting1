7dcc51b846b64f5f6143a6c24065870d51f16ced
Gianluca Formica <gianlucaformica@outlook.com>
2015-09-12 21:58:55 +0100

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,7 +1,9 @@
+## Code below requires that household_power_consumption.txt is in the working directory
 system.time ( hpc <- read.csv.sql(file = "household_power_consumption.txt", sql = "select * from file where Date in ('1/2/2007', '2/2/2007')", header = TRUE, sep = ";") )
 datetime<-as.POSIXct(paste(hpc$Date, hpc$Time), format="%d/%m/%Y %H:%M:%S")
 par(mfrow = c(1, 1))
-##plot1
+
+##This is the plot1 code
 hist(hpc$Global_active_power, 
      main="Global Active Power", 
      xlab="Global Active Power (kilowatts)", 
